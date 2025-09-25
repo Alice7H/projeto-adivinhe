65d71ce314a1849b3ae0d92cddd851f2783ad46b
Alice H. M. Sousa <alicehata9@gmail.com>
2025-09-25 17:51:08 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import styles from './app.module.css'
+import { Header } from './components/Header'
 
 function App() {
 
+  function handleRestart() {
+    console.log('restart the game');
+  }
+
   return (
-    <>
-      <h1>Adivinhe</h1>
-    </>
+    <div className={styles.container}>
+      <main>
+        <Header current={1} max={10} onRestart={handleRestart} />
+      </main>
+    </div>
   )
 }
 
