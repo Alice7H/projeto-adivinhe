2e648181997a8a22af0ead7c0656b8001be4d73f
Alice H. M. Sousa <alicehata9@gmail.com>
2025-09-26 10:38:20 -0300

    feat: sorting the word

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from './app.module.css'
 import { Button } from './components/Button';
 import { Header } from './components/Header'
@@ -5,17 +6,39 @@ import { Input } from './components/Input';
 import { Letter } from './components/Letter';
 import { LettersUsed } from './components/LettersUsed';
 import { Tip } from './components/Tip';
+import { WORDS, type Challenge } from './utils/words';
 
 function App() {
+  const [challenge, setChallenge] = useState<Challenge| null>(null)
+  const [attempts, setAttempts] = useState(0)
+  const [letter, setLetter] = useState("")
+  const [lettersUsed, setLettersUsed] = useState([])
+
 
   function handleRestart() {
     console.log('restart the game');
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+    setChallenge(randomWord) 
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(()=>{
+    startGame()
+  },[])
+
+  if(!challenge){
+    return
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={1} max={10} onRestart={handleRestart} />
+        <Header current={attempts} max={10} onRestart={handleRestart} />
         
         <Tip tip={"Uma das linguagens de programação mais utilizadas"}/>
       
